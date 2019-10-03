0ca85341a9f0883f58ad873a03ebc1820e9bcba3
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-10-03 21:50:03 +0500

    Eliminating an unnecessary character

---
+++ EliminateChar.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim WORDSTR1, WORDSTR2, CH1, NEXTCHAR As String
+        Dim COUNT As Integer
+
+        WORDSTR1 = " "
+        WORDSTR2 = " "
+        CH1 = " "
+        NEXTCHAR = " "
+        COUNT = 0
+
+        Console.Write("Enter String")
+        WORDSTR1 = Console.ReadLine
+
+        Console.Write("Enter character to eliminate: ")
+        CH1 = Console.ReadLine
+
+        For COUNT = 1 To Len(WORDSTR1)
+            NEXTCHAR = Mid(WORDSTR1, COUNT, 1)
+            If NEXTCHAR <> CH1 Then
+                WORDSTR2 = WORDSTR2 & NEXTCHAR
+            End If
+        Next
+
+        Console.WriteLine("Final String = " & WORDSTR2)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
