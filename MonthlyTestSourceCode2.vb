6620c1cb75c16211a1df15a54476344704ab7d10
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-11-14 21:14:36 +0500

    Program assigned for 3rd Monthly Assessment
    This Program basically outputs the recurring alphabet in a string (example : took) it is done by myself completely and it is necessary to read this before checking the actual source code  itself

---
+++ MonthlyTestSourceCode2.vb
@@ -0,0 +1,52 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim WordStr1, NextAlpha, MostRepeatedAlpha, RepairStr As String
+        Dim Counter, PreviousMentionedAlpha, PresentAlpha As Integer
+
+        WordStr1 = " "
+        NextAlpha = " "
+        MostRepeatedAlpha = " "
+        RepairStr = " "
+        Counter = 0
+        PresentAlpha = 0
+        PreviousMentionedAlpha = 1
+
+        Console.Write("Enter the string that you need to be evaluated: ")
+        WordStr1 = Console.ReadLine
+
+        WordStr1 = UCase(WordStr1)
+
+        For Counter = 1 To Len(WordStr1)
+            NextAlpha = Mid(WordStr1, Counter, 1)
+            If NextAlpha = " " Then
+                NextAlpha = " "
+            End If
+            RepairStr = RepairStr & NextAlpha
+        Next
+        WordStr1 = RepairStr
+
+        If WordStr1 <> " " Then
+            For Counter = 1 To Len(WordStr1)
+                NextAlpha = Mid(WordStr1, Counter, 1)
+                Do While InStr(WordStr1, NextAlpha)
+                    PresentAlpha = PresentAlpha + 1
+                    WordStr1 = Left(WordStr1, InStr(WordStr1, NextAlpha) - 1) & Right(WordStr1, Len(WordStr1) - InStr(WordStr1, NextAlpha))
+                Loop
+
+                If PresentAlpha > PreviousMentionedAlpha Then
+                    MostRepeatedAlpha = NextAlpha
+                End If
+                PreviousMentionedAlpha = PresentAlpha
+                PresentAlpha = 0
+            Next
+        End If
+
+        If MostRepeatedAlpha <> " " Then
+            Console.WriteLine("Most repeated alphabet: " & MostRepeatedAlpha)
+        End If
+
+        Console.ReadKey()
+    End Sub
+
+End Module
