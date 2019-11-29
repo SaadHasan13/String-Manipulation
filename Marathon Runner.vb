5627631f3c9e507720d36fc3c0c296c7f475275e
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-11-29 20:47:45 +0500

    Add files via upload

---
+++ Marathon Runner.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim RaceHRS, RaceMINS, RaceSECONDS, RaceTimeInSeconds, PersonalBestTime As Integer
+
+        Console.Write("TIME IN HRS . . . ") : RaceHRS = Console.ReadLine
+        Console.Write("TIME IN MINS . . . ") : RaceMINS = Console.ReadLine
+        Console.Write("TIME IN Secs . . . ")
+        RaceSECONDS = Console.ReadLine
+        Console.Write("Enter Personal Best in seconds . . . ")
+        PersonalBestTime = Console.ReadLine
+        RaceTimeInSeconds = (RaceHRS * 60 * 60) + (RaceMINS * 60) + RaceSECONDS
+        Console.WriteLine(RaceTimeInSeconds)
+
+        If RaceTimeInSeconds < PersonalBestTime Then
+            Console.WriteLine("Your new personal best time")
+        Else
+            If RaceTimeInSeconds = PersonalBestTime Then
+                Console.WriteLine("Your time taken  is equal to the personal best time")
+            Else
+                Console.WriteLine("Personal best time is unchanged")
+            End If
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
