b09f8cfe755c0aa6354d3444cb710cbd37d7f4a8
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-11-29 19:37:11 +0500

    Add files via upload

---
+++ Result Of A Cricket Match.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim HomeTeamName, AwayTeamName, WinningTeamName As String
+        Dim HomeTeamRuns, AwayTeamRuns, RunDiff As Integer
+
+        HomeTeamName = Console.ReadLine
+        HomeTeamRuns = Console.ReadLine
+        AwayTeamName = Console.ReadLine
+        AwayTeamRuns = Console.ReadLine
+
+        If HomeTeamRuns > AwayTeamRuns Then
+            WinningTeamName = HomeTeamName
+        Else
+            WinningTeamName = AwayTeamName
+        End If
+
+        RunDiff = Math.Abs(HomeTeamRuns - AwayTeamRuns)
+
+        Console.WriteLine("The winning team is " & WinningTeamName & " who managed to score " & RunDiff & " more runs")
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
