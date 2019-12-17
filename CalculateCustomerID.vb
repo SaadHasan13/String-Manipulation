4f4abddf4d384ce1a7456bd54a6619175012fec9
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-12-17 21:34:16 +0500

    Add files via upload

---
+++ CalculateCustomerID.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Surname As String
+        Dim SLength, CustomerID, NextCodeNumber, i As Integer
+        Dim NextChar As Char
+
+        Console.Write("Key in surname : ")
+        Surname = Console.ReadLine
+
+        SLength = Len(Surname)
+
+        CustomerID = 0
+
+        For i = 1 To SLength
+            NextChar = Mid(Surname, i, 1)
+            NextCodeNumber = Asc(NextChar)
+            CustomerID = CustomerID + NextCodeNumber
+        Next
+        Console.WriteLine("Customer ID is " & CustomerID)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
