a5f219b2bbd1b9db62acf4cb9a4168e360210596
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2019-12-17 21:39:42 +0500

    Add files via upload

---
+++ CommunicationsLink.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MyString As String
+        Dim StringTotal, I, NextNum As Integer
+
+        Console.Write("Enter a string : ")
+        MyString = Console.ReadLine
+        StringTotal = 0
+
+        For I = 1 To Len(MyString)
+            NextNum = Asc(Mid(MyString, I, 1))
+            StringTotal = StringTotal + NextNum
+        Next
+
+        Console.WriteLine(MyString & " " & Str(StringTotal))
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
